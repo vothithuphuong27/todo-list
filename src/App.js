3be9d71722f51ff3bf287bd23ad60ee310025d2b
Thuphuong27 <thuphuongspdn27@gmail.com>
2021-03-01 19:08:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+import './App.css';
+
+function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'Võ Thị Thu Phương' },
+    { id: 2, title: 'Học reactjs' },
+  ]);
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form submit: ', formValues);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
+
+  return (
+    <div className="app">
+      <h1>List - Todo</h1>
+      { <TodoForm onSubmit={handleTodoFormSubmit} />}
+      { <TodoList todos={todoList} onTodoClick={handleTodoClick} />}
+    </div>
+  );
+}
+export default App;
